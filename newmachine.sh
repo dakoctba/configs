2067eeac4aae51eff9580ff81b99b73a0ea24f49
Jackson Teixeira <jackson.s.teixeira@gmail.com>
2018-05-16 19:04:10 -0300

    Nova maquina

---
+++ newmachine.sh
@@ -0,0 +1,55 @@
+echo "------------------------------"
+echo "Instalação de uma nova máquina Linux"
+echo "------------------------------"
+
+#
+# Para descobrir erros em uma instalacao: sudo dpkg --configure -a
+#
+
+#
+# Atualizacao do sistema
+#
+sudo apt-get update
+sudo apt-get upgrade
+
+#
+# Bibliotecas e dependencias basicas
+#
+sudo apt-get install curl git-core nginx xz-utils libcurl3 -y
+
+#
+# Cria chave ssh
+#
+ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
+
+#
+# Configura o git
+#
+git config --global user.name "Jackson Teixeira"
+git config --global user.email "jackson.s.teixeira@gmail.com"
+git config --global color.ui true
+
+#
+# Docker
+# 
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+#
+# Adiciona o meu usuario no grupo do Docker
+#
+sudo usermod -aG docker jackson
+
+#
+# Recarrega as configuracoes do novo grupo para a sessao atual
+#
+newgrp docker
+
+#
+# Docker compose
+#
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
